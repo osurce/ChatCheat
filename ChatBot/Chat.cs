336fbf6c54968181bcab5a9501c2672256b25d3e
BuildTools <unconfigured@null.spigotmc.org>
2019-10-28 23:02:14 +0100

    Add project files.

---
+++ ChatBot/Chat.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchLib.Client;
+using TwitchLib.Client.Enums;
+using TwitchLib.Client.Events;
+using TwitchLib.Client.Extensions;
+using TwitchLib.Client.Models;
+
+
+namespace ChatBot
+{
+    
+    class chat
+    {
+        static void Main(string[] args)
+        {
+            Bot bot = new Bot();
+            Console.ReadLine();
+        }
+
+    }
+
+    class Bot
+    {
+        TwitchClient client;
+
+        public Bot()
+        {
+            ConnectionCredentials credentials = new ConnectionCredentials("chatcheat", "oauth:df6b12m7fmydpk57jy0namdlek47s5");
+
+            client = new TwitchClient();
+            client.Initialize(credentials, "osurce");
+
+            client.OnLog += Client_OnLog;
+            client.OnJoinedChannel += Client_OnJoinedChannel;
+            client.OnMessageReceived += Client_OnMessageReceived;
+            client.OnConnected += Client_OnConnected;
+
+            client.Connect();
+            
+        }
+
+        private void Client_OnLog(object sender, OnLogArgs e)
+        {
+            Console.WriteLine($"{e.DateTime.ToString()}: {e.BotUsername} - {e.Data}");
+        }
+
+        private void Client_OnConnected(object sender, OnConnectedArgs e)
+        {
+            Console.WriteLine($"Connected to {e.AutoJoinChannel}");
+        }
+
+        private void Client_OnJoinedChannel(object sender, OnJoinedChannelArgs e)
+        {
+            Console.WriteLine("ChatCheat - connected!");
+            client.SendMessage(e.Channel, "ChatCheat - connected!");
+        }
+
+
+
+        private void Client_OnMessageReceived(object sender, OnMessageReceivedArgs e)
+        {
+            if(e.ChatMessage.Message.Contains("test"))
+            {
+
+                string user = e.ChatMessage.DisplayName;
+                string user_id = e.ChatMessage.Id;
+                string message_name = e.ChatMessage.Message;
+
+                //would add chat message to queue
+                ChatCheat.cheat.State.queue.Enqueue(new ChatCheat.cheat.Message(user, user_id, message_name));
+
+                client.SendMessage(e.ChatMessage.Channel, "debug - success");
+            } 
+            
+        }
+
+    }
+
+}
---
+++ ChatCheat/cheat.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GTA;
+using GTA.Native;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Net.Sockets;
+using System.Collections.Concurrent;
+
+namespace ChatCheat
+{
+
+    public class cheat : Script
+    {
+        public static State state = new State();
+
+        private const int bufSize = 8 * 1024;
+
+        public cheat()
+        {
+            state = new State();
+
+            ShowText("ChatCheat Reloaded");
+        }
+        public class State
+        {
+            public static byte[] buffer = new byte[bufSize];
+            public static ConcurrentQueue<string> queue = new ConcurrentQueue<string>();
+
+        }
+
+
+        private void OnTick(object sender, EventArgs e)
+        {
+            CheckQueue();
+            
+        }
+
+        private void CheckQueue()
+        {
+            if(State.queue.IsEmpty)
+            {
+                return;
+            }
+
+            string line;
+            while(State.queue.TryDequeue(out line))
+            {
+                var parts = line.Trim().Split(' ');
+
+                if(parts.Length < 3)
+                {
+                    return;
+                }
+
+                var from = parts[0];
+                var id = parts[1];
+                var name = parts[2];
+
+                Call(from, id, name, parts.Skip(3));
+            }
+        }
+
+        public void Call(String from, String id, String name, IEnumerable<String> rest)
+        {
+            Ped player = Game.Player.Character;
+
+            if(player == null)
+            {
+                return;
+            }
+
+            player.Health = 100;
+        }
+
+        public void ShowText(String text)
+        {
+            GTA.UI.Notification.Show(text);
+        }
+    }
+
+
+}
